93f91af856ce14e11ad82384aa56eb6120f8c429
ShibamRoy9826 <royshibam9826@gmail.com>
2025-03-18 10:27:37 +0530

    Added dark mode toggle, and a drone

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+const scrollDown=document.getElementById("scrollDown");
+const steps=document.getElementById("steps");
+const darkModeToggle=document.getElementById("darkModeToggle");
+const clouds=document.querySelectorAll(".cloud");
+let darkmodeStatus=false;
+
+scrollDown.addEventListener("click",()=>{
+    steps.scrollIntoView({ behavior: "smooth" });
+})
+darkModeToggle.addEventListener("click",()=>{
+    darkmodeStatus=darkModeToggle.classList.toggle("clicked");
+    if(darkmodeStatus){
+        document.documentElement.className="darkMode";
+    }else{
+        document.documentElement.className="";
+    }
+})
+
+document.addEventListener("scroll", function () {
+    let scrollPos = window.scrollY;
+    let plane = document.querySelector("#planeContainer");
+
+    let moveX = scrollPos * 0.5;
+    let moveY = Math.sin(scrollPos * 0.02) * 50;
+
+    plane.style.transform = `translate(${moveX}px, ${moveY}px) rotate(${moveY / 2}deg)`;
+});
+
+clouds.forEach((cloud)=>{
+    let randLeft=Math.random()*100;
+    cloud.style.left=`${randLeft}vw`;
+})
+function animateClouds() {
+    clouds.forEach((cloud) => {
+        let speed= Math.random()*5;
+        let currentLeft = parseFloat(getComputedStyle(cloud).left) || 0;
+        cloud.style.left = `${currentLeft + speed}px`;
+        if (currentLeft > window.innerWidth) {
+            cloud.style.left = "-200px"; 
+        }
+    });
+
+    requestAnimationFrame(animateClouds);
+}
+
+animateClouds();
+
+const observer= new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+const hiddenStuff=document.querySelectorAll(".hidden");
+hiddenStuff.forEach((ele)=>observer.observe(ele));
